919297d57b6b35e4c4003b37176bd9f82e60e5ef
tulasiaparna <74002638+tulasiaparna@users.noreply.github.com>
2023-08-03 00:17:15 +0530

    Update controller.java

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
 	@GetMapping("/")
 	public String hello() {
-		return "Springboot on docker";
+		return "Springboot on docker-automating";
 	}
 
 }
